5d1e027f83cb7b4ecfb23f80f8b9850dfcfc77f2
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-10-29 17:35:32 +0300

    добавлены датчики PMS и MQ-7

--- AirQuality.Sensor/AirQuality.Sensor.ino
+++ AirQuality.Sensor/AirQuality.Sensor.ino
@@ -1,17 +1,27 @@
 #include "WifiConnect.h"
 #include "GetJsonData.h"
 #include "HttpsRequestToServer.h"
-#include <DHT.h>
+
+//DHT22
+#include "DHT.h"
+#define DHTPIN 14   
+#define DHTTYPE DHT22   
+DHT dht(DHTPIN, DHTTYPE);
+
+// PMS7003
+#include "PMS.h"
+#include "SoftwareSerial.h"
+SoftwareSerial pmsSerial(2, 3);// RX, TX
+PMS pms(pmsSerial);
+PMS::DATA data;
+
+int analogMQ7 = A0; // MQ-7
 
 bool wifiIsConnected = false;
 
-
-#define DHTPIN 2
-DHT dht(DHTPIN, DHT11); 
-
-
 void setup() {
   Serial.begin(115200);
+  pmsSerial.begin(9600);
 
   wifiIsConnected = WiFiConnect();
 
@@ -19,22 +29,24 @@ void setup() {
 }
 
 void loop() {
-    if(wifiIsConnected == true){
-      float t = dht.readTemperature(); 
-      float h = dht.readHumidity(); 
-      if (isnan(h) || isnan(t)) {  
-        Serial.println("---DHT Error Read---");
-        return;
-      }
+ if(pms.read(data))
+    {
+      float pm_1 = data.PM_AE_UG_1_0;
+      float pm2_5 = data.PM_AE_UG_2_5;
+      float pm_10 = data.PM_AE_UG_10_0;
 
-      String jsonToPost = GetJsonData(t, h);
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
 
-      SendData(jsonToPost);
-    }else{
-      Serial.print("---WiFi error---");
-    }
+      int co = analogRead(analogMQ7); 
 
-    // задержка на 20 секунд
-    delay(20000);
+      if(wifiIsConnected){
+        String jsonToPost = GetJsonData(t, h, pm_1, pm2_5, pm_10, co);
+
+        SendData(jsonToPost);
+      } 
+
+      delay(60000);
+    }  
 }
 
--- AirQuality.Sensor/GetJsonData.cpp
+++ AirQuality.Sensor/GetJsonData.cpp
@@ -1,15 +1,19 @@
 #include "GetJsonData.h"
 #include <ArduinoJson.h>
 
-String GetJsonData(float t, float h){
+String GetJsonData(float t, float h,float pm_1,float pm2_5,float pm_10,int co){
   String jsonToPost;
   StaticJsonDocument<100> jsonDocument;
 
   jsonDocument["temperature"] = t;
   jsonDocument["humidity"] = h;
+  jsonDocument["pm_1"] = pm_1;
+  jsonDocument["pm2_5"] = pm2_5;
+  jsonDocument["pm_10"] = pm_10;
+  jsonDocument["co"] = co;
   serializeJsonPretty(jsonDocument, jsonToPost);
 
-  Serial.print("---JSON START---");
+  Serial.println("---JSON START---");
   Serial.print(jsonToPost);
   Serial.println("---JSON END---");
 
