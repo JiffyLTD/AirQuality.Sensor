658909a53461e138de83293bbd21c2c3f1d60c3d
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2024-01-06 18:03:50 +0300

    upd all code and add readme

--- AirQuality.Sensor/AirQuality.Sensor.ino
+++ AirQuality.Sensor/AirQuality.Sensor.ino
@@ -7,13 +7,13 @@
 #include <Adafruit_BMP085.h>
 
 //DHT22
-#define DHTPIN 5 //D1  
+#define DHTPIN 5 //D1                         
 #define DHTTYPE DHT22   
 DHT dht(DHTPIN, DHTTYPE);
 
 // PMS7003
-#define TX_PIN_PMS7003 14 //D5
-#define RX_PIN_PMS7003 12 //D6
+#define TX_PIN_PMS7003 14 //D6
+#define RX_PIN_PMS7003 12 //D5
 SoftwareSerial pmsSerial(RX_PIN_PMS7003, TX_PIN_PMS7003);// RX, TX
 PMS pms(pmsSerial);
 PMS::DATA data;
@@ -33,59 +33,187 @@ SoftwareSerial gpsSerial(RX_PIN_GPS, TX_PIN_GPS);
 
 bool wifiIsConnected = false;
 
-unsigned long timing; // Переменная для хранения точки отсчета
+long timing = 0; // Переменная для хранения точки отсчета
 
 void setup() {
+  Serial.begin(115200);
   wifiIsConnected = WiFiConnect();
   pmsSerial.begin(9600);
   gpsSerial.begin(9600);
 
   //BMP180
-  Wire.begin(SDA_PIN_BMP180, SCL_PIN_BMP180);
-  bmp.begin();
+  //Wire.begin(SDA_PIN_BMP180, SCL_PIN_BMP180);
+  //bmp.begin();
   //DHT 22
   dht.begin();
 }
 
 void loop() {
-  if(pms.read(data))
+  if (millis() - timing > 10000) 
   {
-    if (millis() - timing > 60000) 
-    {
-      float pm_1 = data.PM_AE_UG_1_0;
-      float pm2_5 = data.PM_AE_UG_2_5;
-      float pm_10 = data.PM_AE_UG_10_0;
+    float* pmValuesArr = getPMValues();
+    float* dhtValuesArr = getDHTValues();
+    int coValue = getCOValue();
+    //float pressureValue = getPressureValue();
+    String gpsData = getGPSData();    
 
+    String jsonToPost = GetJsonData(dhtValuesArr[1], dhtValuesArr[0], pmValuesArr[0], pmValuesArr[1], pmValuesArr[2], coValue, -1, gpsData);
+
+    delete[] pmValuesArr;
+    delete[] dhtValuesArr;
+
+    if(wifiIsConnected){
+      SendData(jsonToPost);
+    }
+
+    timing = millis();  
+  }
+}
+
+float* getPMValues()
+{
+    try
+    {
+      if(pms.read(data))
+      {
+        float pm_1 = data.PM_AE_UG_1_0;
+        float pm2_5 = data.PM_AE_UG_2_5;
+        float pm_10 = data.PM_AE_UG_10_0;
+
+        float* array = new float[3];
+
+        array[0] = pm_1;
+        array[1] = pm2_5;
+        array[2] = pm_10;
+
+        return array;
+      }
+      else
+      {
+        throw "No pms data";
+      }
+    }
+    catch (...)
+    {
+      float* array = new float[3];
+
+        array[0] = -1;
+        array[1] = -1;
+        array[2] = -1;
+
+        return array;
+    }
+}
+
+float* getDHTValues()
+{
+    try
+    {
       float h = dht.readHumidity();
       float t = dht.readTemperature();
 
+      if (isnan(h) || isnan(t))
+      {
+        throw "No DHT data";
+      }
+
+      float* array = new float[2];
+
+      array[0] = h;
+      array[1] = t;
+
+      return array;
+    }
+    catch (...)
+    {
+      float* array = new float[2];
+
+      array[0] = 0; // Влажность 0%
+      array[1] = -273; // Температура -273°C
+
+      return array;
+    }
+}
+
+int getCOValue()
+{
+    try
+    {
       int co = analogRead(analogMQ7);
 
+      if (co < 0 || co > 1023)
+      {
+        throw "No MQ-7 data";
+      }
+
+      return co;
+    }
+    catch (...)
+    {
+      return -1;
+    }
+}
+
+float getPressureValue()
+{
+    try
+    {
       float pressure = bmp.readPressure();
 
-      String gpsData = "Invalid"; 
+      if (pressure < 0)
+      {
+        throw "No BMP180 data";
+      }
 
+      return pressure;
+    }
+    catch (...)
+    {
+      return -1;
+    }
+}
+
+String getGPSData()
+{
+    try
+    {
       if (gpsSerial.available() > 0)
       {
         String data = "";
+        String gpsData = "";
+        String line = "";
 
-        while(!data.startsWith("$GPGGA")){
-          data = gpsSerial.readStringUntil('\n');
+        while(!line.startsWith("$GPRMC")){
+          line = gpsSerial.readStringUntil('\n');
         }
 
-        gpsData = data; 
-      }      
+        if(line.startsWith("$GPRMC")){
+          gpsData += line;
 
-      String jsonToPost = GetJsonData(t, h, pm_1, pm2_5, pm_10, co, pressure, gpsData);
+          for(int i = 0; i <= 5; i++)
+          {
+            data = gpsSerial.readStringUntil('\n');
 
-      if(wifiIsConnected){
-        SendData(jsonToPost);
+            gpsData += data;
+          } 
+        }
+
+        return gpsData;
       }
-
-      timing = millis();  
+      else
+      {
+        throw "No GPS data";
+      }
     }
-  } 
+    catch (...)
+    {
+      return "Invalid";
+    }
 }
+
+
+
+
 
 
 
