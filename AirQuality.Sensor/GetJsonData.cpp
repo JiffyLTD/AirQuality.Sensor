410797c38e5bf34040ed7dbcec6e5fd8c6d89cbd
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2024-01-20 18:41:06 +0300

    add API token, change URL, change json to post

--- AirQuality.Sensor/GetJsonData.cpp
+++ AirQuality.Sensor/GetJsonData.cpp
@@ -3,16 +3,20 @@
 
 String GetJsonData(float t, float h, float pm_1, float pm2_5, float pm_10, int co, float pressure, String gpsData){
   String jsonToPost;
-  StaticJsonDocument<3000> jsonDocument;
+  StaticJsonDocument<1500> jsonDocument;
 
-  jsonDocument["temperature"] = t;
-  jsonDocument["humidity"] = h;
-  jsonDocument["pm_1"] = pm_1;
-  jsonDocument["pm2_5"] = pm2_5;
-  jsonDocument["pm_10"] = pm_10;
-  jsonDocument["co"] = co;
-  jsonDocument["pressure"] = pressure;
-  jsonDocument["gpsData"] = gpsData;
+  JsonObject createStationDto = jsonDocument.createNestedObject("createStationDto");
+  createStationDto["sensorId"] = sensorId;
+  createStationDto["location"] = gpsData;
+
+  JsonObject createStationDataDto = jsonDocument.createNestedObject("createStationDataDto");
+  createStationDataDto["temperature"] = t;
+  createStationDataDto["humidity"] = h;
+  createStationDataDto["pm_1"] = pm_1;
+  createStationDataDto["pm2_5"] = pm2_5;
+  createStationDataDto["pm_10"] = pm_10;
+  createStationDataDto["co"] = co;
+  createStationDataDto["pressure"] = pressure;
 
   serializeJsonPretty(jsonDocument, jsonToPost);
 
